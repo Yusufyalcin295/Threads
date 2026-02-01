50aec4c2a68bba9d63f5674b9d91143ea6697578
Yusufyalcin295 <ysfyalcin295@gmail.com>
2026-02-01 03:45:38 +0300

    Threads projesi saf kodlar ve README ile güncellendi

---
+++ 05-çoklu_thread.c
@@ -0,0 +1,71 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#define thread_number 3
+
+typedef struct {
+	int id;
+	char* message;
+
+} threadVerisi;
+
+DWORD WINAPI myThreadFunction(LPVOID lpParam) {
+	threadVerisi* veri = (threadVerisi*)lpParam;
+    
+	printf("Thread %d calisiyor!:%s\n", veri->id, veri->message);
+	return 0;
+
+}
+int main() {
+	
+	HANDLE threads[thread_number];
+	threadVerisi* veriler[thread_number];
+
+	//her thread için veri oluşturma
+	int i;
+	for (i = 0; i < thread_number; i++) {
+		veriler[i] = (threadVerisi*)malloc(sizeof(threadVerisi));
+		veriler[i]->id = i + 1;
+
+		if (i == 0)  veriler[i]->message = "ilk gorev";
+		else if (i == 1) veriler[i]->message = "ikinci gorev";
+		else veriler[i]->message = "ucuncu gorev";
+
+
+
+		//threadleri olusturma 
+		threads[i] = CreateThread(NULL, 0, myThreadFunction, veriler[i], 0, NULL);
+
+		if (threads[i] == NULL) {
+			printf("Thread %d oluşturulamadı!\n", i);
+		}
+	}
+	
+	//threadlerin işlerinin bitmesini bekleme
+	
+	WaitForMultipleObjects(thread_number, threads, TRUE, INFINITE);
+	/*
+	* WaitForMultipleObjects(THREAD_SAYISI, threadler, TRUE, INFINITE);
+	| Parametre           | Anlamı                                               |
+| ------------------- | ---------------------------------------------------- |
+| `THREAD_SAYISI` → 3 | Kaç thread varsa o kadar handle bekleyeceğiz         |
+| `threadler`         | `HANDLE threadler[3]` dizisi — beklenen thread’ler   |
+| `TRUE`              | Hepsi tamamlanana kadar bekle (biri değil, **tümü**) |
+| `INFINITE`          | Süre sınırı yok, sonsuza kadar bekle                 |
+
+	*/
+
+	// Temizlik
+	 
+	for (int i = 0; i < thread_number; i++) {
+		CloseHandle(threads[i]);
+		free(veriler[i]);
+		
+	}
+
+	printf("Tum thread'ler tamamlandi.\n");
+	return 0;
+
+
+
+}
---
+++ 06-çoklu_thread_dizi _ortalaması.c
@@ -0,0 +1,162 @@
+﻿
+//HAZIR DİZİ İLE İŞLEM 
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#define MAX_SIZE 100
+#define thread_numbers 3
+
+typedef struct {
+	int id;
+	int* numbers;
+	int size;
+
+}threadVerisi;
+
+DWORD WINAPI  myThreadFunction(LPVOID lpParam) {
+
+	threadVerisi* veri = (threadVerisi*)lpParam;
+
+	int sum = 0;
+
+	for (int i = 0; i < veri->size; i++) {
+		
+		sum += veri->numbers[i];
+ 
+	}
+	double average = (double)sum / veri->size;
+	printf("Thread %d is run ! Average:%.2f \n", veri->id, average);
+
+	return 0;
+ }
+
+int main() {
+
+	HANDLE  threads[thread_numbers];
+	threadVerisi* veriler[thread_numbers];
+
+	int arrays[thread_numbers][5] = { {10,20,30,40,50 }, { 12,14,16,18,20 }, { 0,18,16,34,48 } };
+
+	
+
+	for (int i = 0; i < thread_numbers; i++) {
+
+		veriler[i] = (threadVerisi*)malloc(sizeof(threadVerisi));
+		veriler[i]->id = i + 1;
+		veriler[i]->numbers = arrays[i];
+		veriler[i]->size = 5;
+		
+		threads[i] = CreateThread(NULL, 0, myThreadFunction, veriler[i], 0, NULL);
+
+		if (threads[i] == NULL) {
+			printf("THREAD %d OLUSTURULMADI\n",i);
+		}
+
+
+	}		
+
+	WaitForMultipleObjects(thread_numbers, threads, TRUE, INFINITE);
+
+	for (int i = 0; i < thread_numbers; i++) {
+		CloseHandle(threads[i]);
+		free(veriler[i]);
+
+	}
+
+	printf("PROGRAM SONA ERDI!\n");
+
+
+	return 0;
+}
+*/
+
+//KULLANICIDAN ALININAN BOYUT İLE OLUŞTURULAN DİZİYLE İŞLEM 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#define thread_numbers 3
+
+typedef struct {
+	int id;
+	int* numbers;
+	int size;
+	double average;
+
+}threadVerisi;
+
+DWORD WINAPI myThreadFunction(LPVOID lpParam) {
+	threadVerisi* veri = (threadVerisi*)lpParam;
+	int sum=0;
+
+	for (int i = 0; i <veri->size; i++) {
+
+		sum += veri->numbers[i];
+	}
+	veri-> average = (double)sum / veri->size;
+
+	//printf("Thrad %d is run! average is:%.2f", veri->id, average);
+
+	return 0;
+
+}
+
+int main() {
+
+	HANDLE threads[thread_numbers];
+	threadVerisi* veriler[thread_numbers];
+
+	for (int i = 0; i < thread_numbers; i++) {
+	
+		veriler[i] = (threadVerisi*)malloc(sizeof(threadVerisi));
+		veriler[i]->id = i + 1;
+
+		printf("Enter numbers for thread %d:\n" , i + 1);
+		scanf_s("%d", &veriler[i]->size);
+
+		veriler[i]->numbers = (int*)malloc(sizeof(int) * veriler[i]->size);
+		
+		printf("Enter %d numbers for thread %d :", veriler[i]->size, i + 1);
+
+		for (int j = 0; j < veriler[i]->size; j++) {
+
+			printf("[%d]:", j + 1);
+			scanf_s("%d", &veriler[i]->numbers[j]);
+
+		}
+
+
+		threads[i] = CreateThread(NULL, 0, myThreadFunction, veriler[i], 0, NULL);
+
+
+		if (threads[i] == NULL) {
+			printf("Thread %d is not created!\n");
+		}
+	
+
+	}
+
+	WaitForMultipleObjects(thread_numbers, threads, TRUE, INFINITE);
+
+
+	for (int i = 0; i < thread_numbers; i++) {
+		printf("Average of thread %d is:%.2f\n", veriler[i]->id, veriler[i]->average);
+
+	}
+
+	for (int i = 0; i < thread_numbers; i++) {
+
+		CloseHandle(threads[i]);
+		free(veriler[i]->numbers);
+		free(veriler[i]);
+
+
+	}
+	
+
+	printf("\nAll threads is completed their jobs , program is terminated");
+
+	return 0;
+}
